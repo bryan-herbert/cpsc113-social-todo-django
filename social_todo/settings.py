4bc2351f53e842bcdeee6a0a1596bb16a33f147c
bherbs95 <bryan.herbert@yale.edu>
2016-02-19 15:40:58 +0000

    First commit

--- social_todo/settings.py
+++ social_todo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'splash.apps.SplashConfig',
     'tasks.apps.TasksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
