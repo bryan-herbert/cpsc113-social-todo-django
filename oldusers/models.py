b19b50e6962e60f0138cd944f12ffa7380424ca0
bherbs95 <bryan.herbert@yale.edu>
2016-02-20 22:26:28 +0000

    sha booyah

--- oldusers/models.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-#Should I create a class for users here
-#I don't think so because the django has a default user class
--- oldusers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^register', views.regsiter, name='register'),
-    url(r'^logout', views.logout, name='logout')
-]
-
