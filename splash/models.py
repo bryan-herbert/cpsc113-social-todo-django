ca1722b9d70d0ea341ba4a372cc96b316892f6d5
bherbs95 <bryan.herbert@yale.edu>
2016-02-20 00:42:45 +0000

    Edited views to say that registration form equals register in index.html

--- splash/models.py
+++ splash/models.py
@@ -4,5 +4,3 @@ from django.db import models
 
 # Create your models here.
 
-class RegistrationManager(models.Manager):
-    create_user(
--- splash/views.py
+++ splash/views.py
@@ -9,23 +9,8 @@ from django.http import HttpResponseRedirect
 
 from .forms import RegisterForm
 
-def get_registration(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = RegisterForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            # IMPORTANT- changed it from a different url back to the homepage
-            return HttpResponseRedirect('')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = RegisterForm()
-        
-    return render(request, 'index.html', {'form': form})
+def index(request):
+    registrationForm = RegisterForm();
+     return render(request, 'index.html', {'register': registrationForm})
     
     
